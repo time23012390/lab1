ca905eeafcf039b31ada21731814fdfaf2d01618
Applepi <129792398+time23012390@users.noreply.github.com>
2026-02-10 19:08:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+// let name = "Sofia";
+// let birthYear = 2007;
+// let isStudent = true;
+// console.log(name);
+// console.log(birthYear);
+// console.log(isStudent);
+
+let score = prompt("Введите ваш балл:");
+if (score >= 90) {
+ console.log("Отлично!");
+} else if (score >= 70) {
+ console.log("Хорошо");
+} else {
+ console.log("Можно лучше!");
+}
+
+for (let i = 1; i <= 5; i++) {
+ console.log(`Итерация: ${i}`);
+}
+
+
+
+
+
+
+
+
+
+// let score = prompt("Enter your score: ");
+// if(score >= 90){
+//         console.log("Great!");
+//     } else if(score >= 70){
+//             console.log("Good");
+// } else {
+//         console.log("You can better!");
+// }
+
+// for(let i = 1; i <= 5; i++){
+//         console.log(`Iteration: ${i}`);
+//     }
+    
+    // alert("Этот код выполнен из внешнего файла!");
+    // console.log("Сообщение в консоли");
